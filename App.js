8b7a33e3bdefef0d3080d92bc97cb6a6365e11e7
Neo <ramoroka.neo@gmail.com>
2023-08-01 11:27:21 +0200

    main

--- App.js
+++ App.js
@@ -1,191 +1,263 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Image, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, SafeAreaView, TouchableOpacity, Image, FlatList, StyleSheet } from 'react-native';
+import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
 import * as MediaLibrary from 'expo-media-library';
 import axios from 'axios';
-import DateTimePicker from '@react-native-community/datetimepicker';
 import * as SQLite from 'expo-sqlite';
-const pictures = ('./pictures');
 
-const SERVER_URL = 'http://your_backend_server_url:5000'; // Replace with your backend server URL
-const db = SQLite.openDatabase('uploads.db');
+const db = SQLite.openDatabase('mydatabase.db');
 
-const App = () => {
-  const [media, setMedia] = useState([]);
-  const [showDatePicker, setShowDatePicker] = useState(false);
-  const [selectedDate, setSelectedDate] = useState(new Date());
+const API_KEY = '4NvbKc0C2mimNP8lTUQtF01taGk7H2znVMCgDpLUGbctbqrpV2h2Jh0y';
+
+export default function App() {
+  const [selectedImages, setSelectedImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [isCameraMode, setIsCameraMode] = useState(false);
+  const cameraRef = useRef(null);
+  const thumbnailListRef = useRef(null);
 
   useEffect(() => {
-    createTable();
-    getMedia();
+    (async () => {
+      const { status: cameraStatus } = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus === 'granted');
+
+      const { status: mediaStatus } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (mediaStatus !== 'granted') {
+        console.error('Media library permission denied');
+      }
+
+      createTable();
+      fetchImages();
+      loadImagesFromDatabase();
+    })();
   }, []);
 
   const createTable = () => {
     db.transaction((tx) => {
       tx.executeSql(
-        'CREATE TABLE IF NOT EXISTS files (id INTEGER PRIMARY KEY AUTOINCREMENT, filename TEXT NOT NULL, filepath TEXT NOT NULL, upload_date TEXT NOT NULL);'
+        'CREATE TABLE IF NOT EXISTS images (id INTEGER PRIMARY KEY AUTOINCREMENT, uri TEXT NOT NULL, type TEXT, location TEXT, dateTime TEXT)',
+        [],
+        (_, error) => {
+          if (error) {
+            console.error('Error creating table:', error);
+          } else {
+            console.log('Table created successfully');
+          }
+        }
       );
     });
   };
 
-  const getMedia = async () => {
-    const { status } = await MediaLibrary.requestPermissionsAsync();
-
-    if (status !== 'granted') {
-      alert('Permission to access media library is required.');
-      return;
+  const fetchImages = async () => {
+    try {
+      const response = await axios.get('https://api.pexels.com/v1/curated', {
+        headers: {
+          Authorization: API_KEY,
+        },
+      });
+      const images = response.data.photos.map((photo) => ({
+        id: photo.id,
+        uri: photo.src.large,
+        thumbnailUri: photo.src.tiny,
+        type: 'photo',
+      }));
+      setSelectedImages(images);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching images:', error);
+      setIsLoading(false);
     }
-
-    const mediaAssets = await MediaLibrary.getAssetsAsync({
-      mediaType: [MediaLibrary.MediaType.photo, MediaLibrary.MediaType.video],
-    });
-
-    const results = [];
-    for (const asset of mediaAssets.assets) {
-      const info = await getFileInfo(asset.uri);
-      results.push({ ...asset, ...info });
-    }
-
-    setMedia(results);
   };
 
-  const getFileInfo = async (uri) => {
-    return new Promise((resolve) => {
+  const loadImagesFromDatabase = () => {
+    db.transaction((tx) => {
+      tx.executeSql('SELECT * FROM images', [], (_, result) => {
+        const rows = result.rows;
+        const images = [];
+        for (let i = 0; i < rows.length; i++) {
+          const { uri, type, location, dateTime } = rows.item(i);
+          images.push({
+            id: i,
+            uri,
+            type,
+            location: location ? JSON.parse(location) : null,
+            dateTime: new Date(dateTime),
+          });
+        }
+        setSelectedImages((prevImages) => [...prevImages, ...images]);
+      });
+    });
+  };
+
+  const takePhoto = async () => {
+    if (cameraRef.current) {
+      const photo = await cameraRef.current.takePictureAsync();
+      saveMedia(photo.uri, 'photo');
+    }
+  };
+
+  const recordVideo = async () => {
+    if (cameraRef.current) {
+      const video = await cameraRef.current.recordAsync();
+      saveMedia(video.uri, 'video');
+    }
+  };
+
+  const stopRecordingVideo = async () => {
+    if (cameraRef.current) {
+      cameraRef.current.stopRecording();
+    }
+  };
+
+  const saveMedia = async (uri, type) => {
+    try {
+      const asset = await MediaLibrary.createAssetAsync(uri);
       db.transaction((tx) => {
         tx.executeSql(
-          'SELECT * FROM files WHERE filepath = ?',
-          [uri],
-          (_, { rows }) => {
-            const result = rows.item(0);
-            resolve(result);
+          'INSERT INTO images (uri, type, location, dateTime) VALUES (?, ?, ?, ?)',
+          [uri, type, null, new Date().toISOString()],
+          (_, result) => {
+            if (result.rowsAffected > 0) {
+              console.log('Media saved to database');
+              setSelectedImages((prevImages) => [
+                ...prevImages,
+                { id: prevImages.length, uri, type, location: null, dateTime: new Date() },
+              ]);
+            } else {
+              console.error('Error saving media to database');
+            }
           }
         );
       });
-    });
-  };
-
-  const handlePickMedia = async () => {
-    const permissionCamera = await ImagePicker.requestCameraPermissionsAsync();
-    const permissionLibrary = await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-    if (permissionCamera.status !== 'granted' || permissionLibrary.status !== 'granted') {
-      alert('Both camera and media library permissions are required to take photos and videos.');
-      return;
-    }
-
-    const result = await ImagePicker.launchCameraAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      quality: 1,
-    });
-
-    if (!result.cancelled) {
-      const formData = new FormData();
-      formData.append('file', {
-        uri: result.uri,
-        name: `media.${result.uri.split('.').pop()}`,
-        type: `image/${result.uri.split('.').pop()}`, // Change this to 'video/${...}' for videos
-      });
-
-      try {
-        const response = await axios.post(`${SERVER_URL}/upload`, formData, {
-          headers: { 'Content-Type': 'multipart/form-data' },
-        });
-        console.log('Upload successful. Server response:', response.data);
-
-        const uploadDate = new Date().toISOString();
-        db.transaction((tx) => {
-          tx.executeSql(
-            'INSERT INTO files (filename, filepath, upload_date) VALUES (?, ?, ?)',
-            [response.data.file, result.uri, uploadDate],
-            (_, { insertId }) => {
-              setMedia([...media, { ...result, upload_date: uploadDate, id: insertId }]);
-            },
-            (_, error) => {
-              console.error('Error inserting data into database:', error);
-            }
-          );
-        });
-      } catch (error) {
-        console.error('Error uploading media:', error);
-      }
+    } catch (error) {
+      console.error('Error saving media:', error);
     }
   };
 
-  const renderItem = ({ item }) => {
-    return (
-      <TouchableOpacity onPress={() => viewMedia(item)}>
-        <View>
-          {item.mediaType === 'photo' ? (
-            <Image source={{ uri: item.uri }} style={styles.thumbnail} />
-          ) : (
-            <Text>Video Thumbnail Placeholder</Text>
-          )}
-          <View style={styles.infoContainer}>
-            <Text style={styles.infoText}>Date & Time: {item.takenAt}</Text>
-            <Text style={styles.infoText}>Location: {item.locationInfo}</Text>
-          </View>
-        </View>
+  const switchMode = () => {
+    setIsCameraMode((prev) => !prev);
+  };
+
+  const onLargeListScroll = (event) => {
+    const offsetY = event.nativeEvent.contentOffset.y;
+    thumbnailListRef.current.scrollToOffset({ offset: offsetY, animated: false });
+  };
+
+  const renderCamera = () => (
+    <Camera style={styles.camera} type={Camera.Constants.Type.back} ref={cameraRef}>
+      <TouchableOpacity style={styles.captureButton} onPress={takePhoto}>
+        <Text style={styles.captureButtonText}>Take Photo</Text>
       </TouchableOpacity>
-    );
-  };
+      <TouchableOpacity style={styles.captureButton} onPress={recordVideo}>
+        <Text style={styles.captureButtonText}>Record Video</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.captureButton} onPress={stopRecordingVideo}>
+        <Text style={styles.captureButtonText}>Stop Recording</Text>
+      </TouchableOpacity>
+    </Camera>
+  );
 
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={pictures}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
-        numColumns={3}
-        contentContainerStyle={styles.listContainer}
-      />
-      <TouchableOpacity onPress={handlePickMedia} style={styles.addButton}>
-        <Text style={styles.addButtonText}>+</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      {isCameraMode ? (
+        renderCamera()
+      ) : (
+        <>
+          <FlatList
+            data={selectedImages}
+            renderItem={({ item }) => (
+              <View style={styles.imageContainer}>
+                {item.type === 'photo' ? (
+                  <Image style={styles.image} source={{ uri: item.uri }} />
+                ) : (
+                  <Text style={styles.videoText}>Video</Text>
+                )}
+              </View>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+            ListEmptyComponent={<Text>No media found</Text>}
+            onScroll={onLargeListScroll}
+            scrollEventThrottle={16}
+          />
+          <FlatList
+            ref={thumbnailListRef}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={selectedImages}
+            renderItem={({ item }) => (
+              <TouchableOpacity>
+                {item.type === 'photo' ? (
+                  <Image style={styles.thumbnail} source={{ uri: item.thumbnailUri }} />
+                ) : (
+                  <Text style={styles.videoText}>Video</Text>
+                )}
+              </TouchableOpacity>
+            )}
+            keyExtractor={(item) => item.id.toString()}
+            ListEmptyComponent={<Text>No media found</Text>}
+          />
+        </>
+      )}
+      <TouchableOpacity style={styles.button} onPress={switchMode}>
+        <Text style={styles.buttonText}>{isCameraMode ? 'Exit Camera' : 'Open Camera'}</Text>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  container: {
+  camera: {
     flex: 1,
-    padding: 10,
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
-  listContainer: {
-    flexGrow: 1,
+  captureButton: {
+    backgroundColor: 'white',
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 5,
+  },
+  captureButtonText: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  imageContainer: {
+    margin: 10,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 5,
+  },
+  image: {
+    height: 200,
+    width: '100%',
+    resizeMode: 'cover',
+  },
+  videoText: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 90,
   },
   thumbnail: {
     width: 100,
     height: 100,
     margin: 5,
+    borderRadius: 5,
   },
-  addButton: {
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
-    backgroundColor: 'blue',
-    width: 50,
+  button: {
+    marginTop: 20,
     height: 50,
-    borderRadius: 25,
+    width: '60%',
+    backgroundColor: 'skyblue',
+    borderRadius: 20,
     justifyContent: 'center',
     alignItems: 'center',
+    alignSelf: 'center',
   },
-  addButtonText: {
-    color: '#FFF',
-    fontSize: 24,
-  },
-  infoContainer: {
-    padding: 5,
-    backgroundColor: 'rgba(0, 0, 0, 0.8)',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-  },
-  infoText: {
-    color: '#FFF',
-    fontSize: 12,
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
-
-export default App;
